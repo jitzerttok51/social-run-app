1b990b016e9893f41c8f0095a15dcb638b2c0723
Nikolay Prodanov <nikolay.prodanov@yahoo.com>
2024-07-11 11:49:07 +0300

    Initial commit

---
+++ src/main/java/org/jitzerttok51/social/run/SocialRunAppApplication.java
@@ -0,0 +1,13 @@
+package org.jitzerttok51.social.run;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SocialRunAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SocialRunAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/jitzerttok51/social/run/SocialRunAppApplicationTests.java
@@ -0,0 +1,13 @@
+package org.jitzerttok51.social.run;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SocialRunAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
