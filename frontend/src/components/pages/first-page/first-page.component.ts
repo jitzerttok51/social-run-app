4c8f4534d7d58c200b65b825d66b0ebd6d6e5158
Nikolay Prodanov <nikolay.prodanov@yahoo.com>
2024-07-13 00:17:22 +0300

    Added basic registration page

--- frontend/src/components/pages/first-page/first-page.component.ts
+++ frontend/src/components/pages/first-page/first-page.component.ts
@@ -1,8 +1,15 @@
 import { Component } from '@angular/core';
+import { MatCardModule } from '@angular/material/card'
+import { MatFormFieldModule } from '@angular/material/form-field'
+import { MatCheckboxModule } from '@angular/material/checkbox'
+import { MatRadioModule } from '@angular/material/radio'
+import { MatInputModule } from '@angular/material/input'
+import { MatButtonModule } from '@angular/material/button'
+import { MatDividerModule } from '@angular/material/divider'
 @Component({
   selector: 'first-page',
   standalone: true,
-  imports: [],
+  imports: [MatCardModule, MatDividerModule, MatFormFieldModule, MatInputModule, MatButtonModule, MatRadioModule, MatCheckboxModule],
   templateUrl: './first-page.component.html',
   styleUrl: './first-page.component.scss'
 })
