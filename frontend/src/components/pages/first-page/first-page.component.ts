5bdd3391483b36cf7df839189102dc780704888d
Nikolay Prodanov <nikolay.prodanov@yahoo.com>
2024-07-13 00:35:37 +0300

    Includa additional fields

--- frontend/src/components/pages/first-page/first-page.component.ts
+++ frontend/src/components/pages/first-page/first-page.component.ts
@@ -6,10 +6,14 @@ import { MatRadioModule } from '@angular/material/radio'
 import { MatInputModule } from '@angular/material/input'
 import { MatButtonModule } from '@angular/material/button'
 import { MatDividerModule } from '@angular/material/divider'
+import { MatDatepickerModule } from '@angular/material/datepicker'
+import {provideNativeDateAdapter, MAT_DATE_LOCALE} from '@angular/material/core';
+
 @Component({
   selector: 'first-page',
   standalone: true,
-  imports: [MatCardModule, MatDividerModule, MatFormFieldModule, MatInputModule, MatButtonModule, MatRadioModule, MatCheckboxModule],
+  providers: [provideNativeDateAdapter(), { provide: MAT_DATE_LOCALE, useValue: 'en-GB' }],
+  imports: [MatCardModule, MatDatepickerModule, MatDividerModule, MatFormFieldModule, MatInputModule, MatButtonModule, MatRadioModule, MatCheckboxModule],
   templateUrl: './first-page.component.html',
   styleUrl: './first-page.component.scss'
 })
