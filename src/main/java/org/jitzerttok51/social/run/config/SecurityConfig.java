875f7792ef9061d3427183683dc106e4c8b6ac6f
Nikolay Prodanov <nikolay.prodanov@yahoo.com>
2024-07-15 00:31:27 +0300

    Added spring security (#17)

---
+++ src/main/java/org/jitzerttok51/social/run/config/SecurityConfig.java
@@ -0,0 +1,39 @@
+package org.jitzerttok51.social.run.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return PasswordEncoderFactories.createDelegatingPasswordEncoder();
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        return http.cors(AbstractHttpConfigurer::disable)
+                .csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(x -> x
+                        .requestMatchers(HttpMethod.POST, "/api/auth", "/api/users").permitAll()
+                        .requestMatchers(HttpMethod.GET, "/storage/**").permitAll() // Images serving endpoint
+                        .requestMatchers("/api/**").authenticated()
+                        .requestMatchers(HttpMethod.GET,"/**").permitAll() // UI serving endpoint
+                        .requestMatchers("/error").permitAll() // UI serving endpoint
+                        .anyRequest().authenticated())
+//                .sessionManagement(s -> s.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+//                .addFilterBefore(filter, UsernamePasswordAuthenticationFilter.class)
+//                .httpBasic(AbstractHttpConfigurer::disable)
+                .anonymous(AbstractHttpConfigurer::disable)
+                .build();
+    }
+}
